f696f15afdc869e7f4ddaf202aedb8a7586a491a
Yakihavi <84100345+Yakihavi@users.noreply.github.com>
2021-07-28 19:14:48 +0300

    Add files via upload

---
+++ dz_3_1.py
@@ -0,0 +1,9 @@
+def num_translite(eng):
+    nums = {'zero': 'ноль', 'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять',
+            'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять'}
+    return nums.get(eng)
+
+
+print(num_translite('one'))
+print(num_translite('eight'))
+
